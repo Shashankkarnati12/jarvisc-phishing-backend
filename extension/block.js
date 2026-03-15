cff495f6326f40942e2ed34c5876e143a9768f27
Shashank <karnatishashank356@gmail.com>
2026-03-15 21:27:51 +0530

    Removed VirusTotal fields from block page

--- extension/block.js
+++ extension/block.js
@@ -4,8 +4,6 @@ document.getElementById("risk").innerText = params.get("risk") || "N/A";
 document.getElementById("ml").innerText = params.get("ml") || "N/A";
 document.getElementById("ssl").innerText = params.get("ssl") || "N/A";
 document.getElementById("age").innerText = params.get("age") || "N/A";
-document.getElementById("vtm").innerText = params.get("vtm") || "0";
-document.getElementById("vts").innerText = params.get("vts") || "0";
 
 document.getElementById("backBtn").addEventListener("click", () => {
     history.back();
