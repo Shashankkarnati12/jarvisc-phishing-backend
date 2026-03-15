df8d6bff741519a6d2f57ea9bc1a1c322da219b0
Shashank <karnatishashank356@gmail.com>
2026-03-15 17:10:11 +0530

    Added phishing block page details and block.js fix

---
+++ extension/block.js
@@ -0,0 +1,8 @@
+const params = new URLSearchParams(window.location.search);
+
+document.getElementById("risk").innerText = params.get("risk") || "N/A";
+document.getElementById("ml").innerText = params.get("ml") || "N/A";
+document.getElementById("ssl").innerText = params.get("ssl") || "N/A";
+document.getElementById("age").innerText = params.get("age") || "N/A";
+document.getElementById("vtm").innerText = params.get("vtm") || "0";
+document.getElementById("vts").innerText = params.get("vts") || "0";
