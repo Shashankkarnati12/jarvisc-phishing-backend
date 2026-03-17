509e912830d1b629db01e8ef5e8f7b582018cf61
Shashank <karnatishashank356@gmail.com>
2026-03-17 10:42:17 +0530

    final fix

--- extension/content.js
+++ extension/content.js
@@ -6,30 +6,30 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 
         const box = document.createElement("div");
 
-        box.innerHTML = `
-        <div style="
-        position:fixed;
-        bottom:20px;
-        right:20px;
-        background:#2ecc71;
-        color:white;
-        padding:14px;
-        border-radius:8px;
-        font-size:13px;
-        z-index:999999;
-        width:220px;
-        box-shadow:0 0 10px rgba(0,0,0,0.3);
-        ">
+      box.innerHTML = `
+<div style="
+position:fixed;
+bottom:20px;
+right:20px;
+background:#2ecc71;
+color:white;
+padding:14px;
+border-radius:8px;
+font-size:13px;
+z-index:999999;
+width:220px;
+box-shadow:0 0 10px rgba(0,0,0,0.3);
+">
 
-        <b>&#9989; SAFE WEBSITE</b>
+<b>✅ SAFE WEBSITE</b><br><br>
 
-        Risk Score: ${data.risk_score}<br>
-        ML Probability: ${data.probability}<br>
-        SSL Status: ${data.ssl_status}<br>
-        Domain Age: ${data.domain_age_days} days<br>
+Risk Score: ${data.risk_score}<br>
+ML Probability: ${data.probability}<br>
+SSL Status: ${data.ssl_status}<br>
+Domain Age: ${data.domain_age_days} days
 
-        </div>
-        `;
+</div>
+`;
 
         document.body.appendChild(box);
 
